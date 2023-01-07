c9c5001a63ba3190373228a6f88ae26c77dd64d6
seanbruceful <seanbruce520@gmail.com>
2023-01-07 17:27:16 +0800

    :feat in-memory dbContext

--- Pizza.cs
+++ Pizza.cs
@@ -1,9 +1,18 @@
 ﻿using System;
+using Microsoft.EntityFrameworkCore;
 namespace PizzaStore.Modals
 {
 	public class Pizza
 	{
 		public int Id { get; set; }
+		public string? Name { get; set; }
+		public string? Description { get; set; }
+	}
+
+	class PizzaDb : DbContext
+	{
+		public PizzaDb(DbContextOptions options) : base(options) { }
+		public DbSet<Pizza> Pizzas { get; set; } = null!;
 	}
 }
 
