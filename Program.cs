7b52a723acd25d6009e363c5add604b72efd36c9
seanbruceful <seanbruce520@gmail.com>
2023-01-07 17:52:58 +0800

    :feat SQLite database

--- Program.cs
+++ Program.cs
@@ -3,9 +3,11 @@ using Microsoft.EntityFrameworkCore;
 using PizzaStore.Modals;
 
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("Pizzas") ?? "Data Source=Pizzas.db";
+
 
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddDbContext<PizzaDb>(options => options.UseInMemoryDatabase("items"));
+builder.Services.AddSqlite<PizzaDb>(connectionString);
 
 builder.Services.AddSwaggerGen(c =>
 {
